55ce43ca3caec96c1ec0f89844a9e1ffc479595f
Pontus <contrib@pontusnetworks.com>
2017-11-08 21:45:20 +0000

    added es, gui and server services

--- build-local.sh
+++ build-local.sh
@@ -2,7 +2,7 @@
 
 VERSION=0.13.0-SNAPSHOT
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 
 
 DEST_DIR=$DIR/../pontus-dist/opt/pontus/pontus-knox
@@ -24,5 +24,7 @@ ln -s knox-${VERSION}/ current
 
 cd current
 ./bin/knoxcli.sh create-master --generate
+
+tar xvfz $DIR/conf.tar.gz
 
 cd $CURDIR
